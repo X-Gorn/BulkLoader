be7d72f815065272600c9c1de204d22be54da697
X-Gorn <xgorntv@gmail.com>
2024-06-07 12:12:44 +0900

    docs: update helper

--- Bot/plugins/commands.py
+++ Bot/plugins/commands.py
@@ -15,9 +15,9 @@ reply_markup = InlineKeyboardMarkup([
 
 @Client.on_message(filters.command('start') & OWNER_FILTER & filters.private)
 async def start(bot: Client, update: Message):
-    await update.reply_text(text=f"I'm BulkLoader\nYou can upload list of urls\n\n/help for more details!", quote=True, reply_markup=reply_markup)
+    await update.reply_text(text=f"I'm BulkLoader\nYou can upload list of links\n\n/help for more details!", quote=True, reply_markup=reply_markup)
 
 
 @Client.on_message(filters.command('help') & OWNER_FILTER & filters.private)
 async def help(bot: Client, update: Message):
-    await update.reply_text(text=f"How to use BulkLoader?!\n\n2 Methods:\n- send command /link and then send urls, separated by new line.\n- send txt file (links), separated by new line.", quote=True, reply_markup=reply_markup)
+    await update.reply_text(text=f"How to use BulkLoader?!\n\n2 Methods:\n- send a bunch of links.\n- send text file which contains links.\n\nNote: Make sure that each link is separated.", quote=True, reply_markup=reply_markup)
