4f69079b1640d991f362c078e0815f8626de9629
X-Gorn <xgorntv@gmail.com>
2024-06-07 14:50:31 +0900

    docs: update

--- Bot/plugins/commands.py
+++ Bot/plugins/commands.py
@@ -20,4 +20,4 @@ async def start(bot: Client, update: Message):
 
 @Client.on_message(filters.command('help') & OWNER_FILTER & filters.private)
 async def help(bot: Client, update: Message):
-    await update.reply_text(text=f"How to use BulkLoader?!\n\n2 Methods:\n- send a bunch of links.\n- send text file which contains links.\n\nNote: Make sure that each link is separated.", quote=True, reply_markup=reply_markup)
+    await update.reply_text(text=f"How to use BulkLoader?!\n\nMethods:\n- send a bunch of links.\n- send text file which contains links.\n\nNote: Make sure that each link is separated.\n\nSettings\n\n- `/thumbnail`: custom thumbnail. ex: reply to a photo or do `/thumbnail https...jpg`\n- `/caption`: custom thumbnail. ex: `/caption abc`\n\nNote: To clear thumbnail or the caption. do the command without args. ex: `/thumbnail` or `/caption`", quote=True, reply_markup=reply_markup)
