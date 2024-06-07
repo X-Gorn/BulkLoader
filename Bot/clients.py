1d11560af6bb0f03850c5fb09344ce815ec3721f
X-Gorn <xgorntv@gmail.com>
2024-06-07 11:24:10 +0900

    fix: clients not found

--- Bot/clients.py
+++ Bot/clients.py
@@ -4,6 +4,7 @@ from pyrogram import Client
 
 
 class BotClient(Client):
+
     def __init__(self):
         self.bot: Client = Client(name='BulkLoader', api_id=Config.APP_ID, api_hash=Config.API_HASH,
                                   bot_token=Config.BOT_TOKEN, plugins=Config.PLUGINS)
